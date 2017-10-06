b47e595bdac7f21fa60a4bb032f284c163bf8423
catsl_000 <catsl_000@192.168.0.6>
2017-10-06 15:09:54 -0500

    7 out of the 10 cases are done, one of them is working though.

---
+++ War/src/TestCases.java
@@ -0,0 +1,84 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.Stack;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCases {
+	protected CardRank rank = CardRank.ACE;
+	protected CardSuit suit = CardSuit.HEARTS;
+	protected Card card;
+	protected Player player;
+	protected String playerName = "Test Name";
+	protected Hand playerHand;
+	protected int score = 0;
+	protected Stack<Card> cards;
+	
+	@Before
+	public void setUpCard()
+	{
+		card = new Card(rank, suit);
+	}
+	
+	@Before
+	public void setUpPlayer()
+	{
+		player = new Player(playerName);
+	}
+	
+	@Before
+	public void setUpHand()
+	{
+		cards = new Stack<Card>();
+		playerHand = new Hand();
+		playerHand.push(card);
+		cards.push(card);
+	}
+	
+	@Test
+	public void testGetRank()
+	{
+		assertEquals(rank, card.getRank());
+	}
+	
+	@Test
+	public void testGetSuit()
+	{
+		assertEquals(suit, card.getSuit());
+	}
+	
+	@Test
+	public void testGetPlayerName()
+	{
+		assertEquals(playerName, player.getPlayerName());
+	}
+	
+	@Test
+	public void testGetPlayerHand()
+	{
+		//player.addCardToHand(card);
+		assertEquals(playerHand, player.getPlayerHand());
+	}
+	
+	@Test
+	public void testGetScore()
+	{
+		player.addPoints(score);
+		assertEquals(score, player.getScore());
+	}
+	
+	@Test
+	public void testIsEmpty()
+	{
+		assertFalse(playerHand.isEmpty());
+	}
+	
+	@Test
+	public void testSize()
+	{
+		assertEquals(cards.size(), playerHand.size());
+	}
+
+}
